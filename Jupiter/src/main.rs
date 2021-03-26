a2f59dc8a40758a607c8fede4dabacd5068962c3
thespragg <alistair.spragg@gmail.com>
2021-03-26 23:44:03 +0000

    Altered main

--- Jupiter/src/main.rs
+++ Jupiter/src/main.rs
@@ -1,17 +1,6 @@
-mod block;
-use block::Block;
-mod transaction;
-use transaction::Transaction;
+use jupiterlib::*;
 
 fn main() {
-    let block = Block{
-        index: 0,
-        timestamp: 0,
-        transactions: Vec::new(),
-        hash: Vec::new(),
-        prev_block_hash: Vec::new(),
-        validator: String::new(),
-    };
-
+    let block = Block::generate_genesis();
     println!("{:?}",block)
 }
